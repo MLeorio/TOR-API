83da66e7280974104d1b018d8acb8056e600459a
Fernand AFANOU <fernandafanou@gmail.com>
2024-01-19 02:14:19 +0000

    Revisioin de la base de donnees

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
+from email.policy import default
 from tortoise import fields, models
 from tortoise.contrib.pydantic import pydantic_model_creator
 from datetime import datetime
+
 
 class Annonce(models.Model):
     id = fields.IntField(pk=True)
@@ -12,11 +14,20 @@ class Annonce(models.Model):
     telephone_person_to_contact = fields.CharField(max_length=12)
     place_of_loss_or_find = fields.CharField(max_length=255)
     date_of_loss_or_find = fields.DateField()
-    
 
-    statut = fields.CharField(max_length=50, ) # Statut de l'annonce (soit trouver ou perdu)
-    actif = fields.IntField(default=1) # avertit si l'annonce est toujours actif ou pas (l'objet a retrouve son propretaire) 1=Oui 0=Non
+    """
+        Revoir la base de donnees: Ambigute dans le nom des colonnes
+        - statut doit plutot etre etat evoquant le fait que l'objet soit en possession du proporietaire ou pas
+        valeur possible : full = proprietaire a la recherche de son objet
+                        pupa = porprietaire a retrouver son objet
 
+        Solution:
+        - la colonne statut doit prendre les valeur "full" et "pupa"
+        - ajouter une nouvelle colonne "type" qui represente le type de l'annonce a savoir "trouve" ou "perdu"
+    """
+    type = fields.CharField(max_length=100)
+    statut = fields.CharField(max_length=50, default="full") 
+    actif = fields.IntField()  # avertit si l'annonce est publiee ou pas
 
     created_at = fields.DatetimeField(auto_now_add=True, default=datetime.now())
     updated_at = fields.DatetimeField(auto_now=True, default=datetime.now())
@@ -25,17 +36,18 @@ class Annonce(models.Model):
         return self.libelle_objet
 
 
-
 class Device(models.Model):
     id = fields.BigIntField(pk=True)
     device_id = fields.CharField(max_length=255)
     installation_date = fields.DatetimeField(auto_now_add=True, default=datetime.now())
 
 
-
-
 Annonce_Pydantic = pydantic_model_creator(Annonce, name="Annonce")
-AnnonceIn_Pydantic = pydantic_model_creator(Annonce, name="AnnonceIn", exclude_readonly=True)
+AnnonceIn_Pydantic = pydantic_model_creator(
+    Annonce, name="AnnonceIn", exclude_readonly=True
+)
 
 Device_Pydantic = pydantic_model_creator(Device, name="Device")
-DeviceIn_Pydantic = pydantic_model_creator(Device, name="DeviceIn", exclude_readonly=True)
+DeviceIn_Pydantic = pydantic_model_creator(
+    Device, name="DeviceIn", exclude_readonly=True
+)
